c781d5b83593bf827e8044a8cbc726ed168e2f20
Shubham Vishwakarma <shubhamvishwakarma0604@gmail.com>
2023-05-14 15:57:20 +0530

    Bug Fixes

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-const PDFExtract = require('pdf.js-extract').PDFExtract;
-const pdfExtract = new PDFExtract();
-const { log } = require('console');
-const fs = require('fs');
-const buffer = fs.readFileSync("./hech-333.pdf");
-const options = {}; /* see below */
-pdfExtract.extractBuffer(buffer, options, (err, data) => {
-  if (err) return console.log(err);
-    pages=data.pages;
-    tempData='';
-    pages.forEach((page, index) => {
-        console.log(`Page ${index + 1}`);
-        page.content.forEach((element) => {
-            // extract text from 'Destination Code' to 'Pickup'
-            if (element.str.includes('Destination Code')) {
-                // read until 'Pickup'
-                let i = 1;
-                while (!page.content[index + i].str.includes('Pickup')) {
-                    tempData+=page.content[index + i].str;
-                    i++;
-                }
-            }
+const PDFExtract = require("pdf.js-extract").PDFExtract;
 
-        });
-        // trim the tempData from 'Destination Code' to the end
-        tempData=tempData.substring(tempData.indexOf('Destination Code'));
-        console.log(tempData);
-        // reset tempData
-        tempData='';
+const pdf = require("pdf-parse");
+
+const pdfExtract = new PDFExtract();
+const fs = require("fs");
+const dataBuffer = fs.readFileSync("./hech-333.pdf");
+
+pdf(dataBuffer).then(function (data) {
+  // number of pages
+  console.log(data.numpages);
+  // PDF text
+  let s = data.text;
+  let xa = s.split("\n");
+  console.log(xa);
+  xa.filter((x, index) => {
+    if (x == "Pickup") {
+      console.log(index);
+      console.log(xa[index - 1]);
+      console.log(xa[index - 1] === undefined);
     }
-    );
+  });
 });
+
+/* LEGACY CODE */
+
+/* see below */
+// pdfExtract.extractBuffer(buffer, options, (err, data) => {
+//   if (err) return console.log(err);
+//   pages = data.pages;
+//   tempData = "";
+//   pages.forEach((page, index) => {
+//     console.log(page);
+//     console.log(`Page ${index + 1}`);
+//     page.content.forEach((element) => {
+//       //   console.log(element);
+//       // extract text from 'Destination Code' to 'Pickup'
+//       //   if (element.str.includes("Destination Code")) {
+//       //     // read until 'Pickup'
+//       //     let i = 1;
+//       //     while (!page.content[index + i].str.includes("Pickup")) {
+//       //       tempData += page.content[index + i].str;
+//       //       i++;
+//       //     }
+//       //   }
+//       // });
+//       // // trim the tempData from 'Destination Code' to the end
+//       // tempData = tempData.substring(tempData.indexOf("Destination Code"));
+//       // console.log(tempData);
+//       // // reset tempData
+//       // tempData = "";
+//     });
+//   });
+// });
+
+/* LEGACY CODE */
